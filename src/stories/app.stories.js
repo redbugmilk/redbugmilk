ef0a0fec8b93344ded63b2bc681d54ff6f082ad6
joana.correia <joana.correia@truphone.com>
2020-04-26 16:18:25 +0100

    Revert "revert deploy on development"
    This reverts commit 421eccd36190009ed56512324c17e59475cfb9a4.

--- src/stories/app.stories.js
+++ src/stories/app.stories.js
@@ -1,81 +1,86 @@
-// import React from 'react';
-// import { Container, Paper, CssBaseline, Box, Slide, } from '@material-ui/core';
-// import MainMenu from "../components/MainMenu";
-// import { generateMenuInformation } from "../mapping/MainMenu";
-// import { makeStyles } from '@material-ui/core/styles';
-// import Experience from '../controllers/Experience';
-// import PersonalAvatar from '../components/Personal.avatar';
+import React from 'react';
+import { Container, Paper, CssBaseline, Box, Divider } from '@material-ui/core';
+import MainMenu from "../components/MainMenu";
+import { generateMenuInformation } from "../mapping/MainMenu";
+import { makeStyles } from '@material-ui/core/styles';
+import Experience from '../controllers/Experience';
 
-// const useStyles = makeStyles(theme => ({
-//     App: {
-//         display: "flex",
-//         flex: 1
-//     },
-//     paper: {
-//         display: "flex",
-//         flexDirection: "column",
-//         justifyContent: "center",
-//     },
-//     menu: {
-//         display: "flex",
-//         justifyContent: "center",
-//         backgroundColor: theme.palette.common.white,
-//         marginBottom: theme.spacing(2),
-//     },
-//     experience: {
-//         display: "flex",
-//         paddingLeft: theme.spacing(4),
-//         justifyContent: "center",
-//         alignItems: "center",
-//         backgroundColor: theme.palette.common.white,
-//         marginBottom: theme.spacing(4),
-//     },
+const useStyles = makeStyles(theme => ({
+    App: {
+        display: "flex",
+        flex: 1
+    },
+    paper: {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent:"center",
+    },
+    imageMain: {
+        position: 'relative',
+        backgroundColor: theme.palette.grey[800],
+        color: theme.palette.common.white,
+        marginBottom: theme.spacing(4),
+        backgroundImage: 'url(https://source.unsplash.com/user/erondu)',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+    },
+    menu: {
+        display: "flex",
+        justifyContent: "center",
+        backgroundColor: theme.palette.common.white,
+        marginBottom: theme.spacing(2),
+    },
+    experience: {
+        display: "flex",
+        paddingLeft: theme.spacing(4),
+        justifyContent: "center",
+        alignItems:"center",
+        backgroundColor: theme.palette.common.white,
+        marginBottom: theme.spacing(4),
+    },
+}));
 
-// }));
+function App() {
+    const classes = useStyles();
+    const [menuIndex, setMenuIndex] = React.useState(0);
+    const handleClick = (id) => {
+        setMenuIndex(id);
+    };
+    return (
+        <div className={classes.App}>
+            <CssBaseline />
+            <Container maxWidth="lg">
+                <main>
+                    <Paper className={classes.paper} >
+                        <Box 
+                            height={200} 
+                            className={classes.imageMain} 
+                            boxShadow={3}
+                        />
+                        <Box className={classes.menu}>
+                            <MainMenu
+                                menus={generateMenuInformation()}
+                                onClick={handleClick}
+                                menuSelected={menuIndex}
+                            />
+                        </Box>
+                        <Box className={classes.experience}>
+                            <Experience />
+                        </Box>
+                    </Paper>
+                </main>
+            </Container>
+        </div>
+    );
+}
 
-// function App() {
-//     const classes = useStyles();
-//     const [menuIndex, setMenuIndex] = React.useState(0);
-//     const [checked, setChecked] = React.useState(false);
-//     const handleClick = (id) => {
-//         setMenuIndex(id);
-//     };
-//     const handleChange = () => {
-//         setChecked(prev => !prev);
-//     };
-//     return (
-//         <div className={classes.App}>
-//             <CssBaseline />
-//             <Container maxWidth="lg">
-//                 <main>
-//                     <Paper className={classes.paper} >
-//                         <PersonalAvatar onClick={handleChange} />
-//                         <Slide direction="up" in={checked} mountOnEnter unmountOnExit>
-                            
-//                         </Slide>
-//                         <Box className={classes.menu}>
-//                             <MainMenu
-//                                 menus={generateMenuInformation()}
-//                                 onClick={handleClick}
-//                                 menuSelected={menuIndex}
-//                             />
-//                         </Box>
-//                         <Box className={classes.experience}>
-//                             <Experience />
-//                         </Box>
-//                     </Paper>
-//                 </main>
-//             </Container>
-//         </div>
-//     );
-// }
+export default {
+    title: 'App wrapper',
+};
 
-// export default {
-//     title: 'App wrapper',
-// };
+export const toStorybook = () => <App />;
 
-// export const toStorybook = () => <App />;
-
-// toStorybook.story = {
-//     name: 'App wrapper',
-// };
+toStorybook.story = {
+    name: 'App wrapper',
+};
