eab1fa64f2ff21f7b08bc79f71bc5859e8bc185b
redbugmilk <redbugmilk@gmail.com>
2020-11-12 21:52:07 +0000

    update main menu mapper

--- src/mapping/MainMenu.js
+++ src/mapping/MainMenu.js
@@ -3,13 +3,13 @@ import { MenuBook, Work, Face } from "@material-ui/icons";
 
 const mappingMenuInformation = (icon, label) => ({
   icon,
-  label
+  label,
 });
 
 const variant = {
   Experience: <Work fontSize="small" color="action" />,
   Education: <MenuBook fontSize="small" color="action" />,
-  Personal: <Face fontSize="small" color="action" />,
+  // Personal: <Face fontSize="small" color="action" />,
 };
 const generateMenuInformation = () =>
   Object.entries(variant).map(([key, value]) =>
