12d3ee2bf5dcc1430a4bd21dbda52429c1bce9b5
redbugmilk <redbugmilk@gmail.com>
2022-08-23 12:57:37 +0100

    revert previous code

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <p>testing</p>
+    </div>
+  );
+}
+export default App;
--- src/stories/personal.stories.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Personal from '../controllers/Personal';
-
-export default {
-    title: 'Personal wrapper',
-    component: Personal
-};
-
-export const Template = () => <Personal />;
